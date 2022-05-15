47cfe6e7811d2c8d0812e22e026107aedd52cba8
Misha <msmirnov10000@gmail.com>
2022-05-15 03:59:08 -0400

    misc stuff

---
+++ src/main/java/com/misha/items/CopperGloves.java
@@ -0,0 +1,36 @@
+package com.misha.items;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+public class CopperGloves extends Item implements ICurioItem {
+
+
+    public CopperGloves(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag flags) {
+        super.appendHoverText(stack, level, list, flags);
+        list.add(new TranslatableComponent("message.coppergloves").withStyle(ChatFormatting.DARK_GRAY));
+    }
+
+
+}
--- src/main/java/com/misha/lavaplus/LavaPlus.java
+++ src/main/java/com/misha/lavaplus/LavaPlus.java
@@ -4,11 +4,15 @@ import com.misha.setup.ClientSetup;
 import com.misha.setup.Registration;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(LavaPlus.MODID)
@@ -22,12 +26,16 @@ public class LavaPlus
         Registration.init();
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::setup);
+        bus.addListener(this::enqueueIMC);
         bus.addListener(ClientSetup::setup);
-        //bus.addListener(ClientSetup::setup);
 
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void enqueueIMC(final InterModEnqueueEvent event){
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE,
+                ()-> SlotTypePreset.HANDS.getMessageBuilder().build());
     }
 
     private void setup(final FMLCommonSetupEvent event)
