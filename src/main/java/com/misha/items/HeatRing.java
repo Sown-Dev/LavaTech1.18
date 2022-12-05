ec26821d904703a1c6fd2209a7915f7c8bfd8a46
Misha <msmirnov10000@gmail.com>
2022-12-05 17:29:35 -0500

    Balance changes

---
+++ src/main/java/com/misha/items/HeatRing.java
@@ -0,0 +1,45 @@
+package com.misha.items;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+public class HeatRing extends Item implements ICurioItem {
+
+
+    public HeatRing(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag flags) {
+        super.appendHoverText(stack, level, list, flags);
+        // list.add(new TranslatableComponent("message.basiccharm").withStyle(ChatFormatting.DARK_GRAY));
+    }
+
+    @Override
+    public int getItemStackLimit(ItemStack stack) {
+        return 1;
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(Attributes.ARMOR, new AttributeModifier(uuid, "Armor", 2F, AttributeModifier.Operation.ADDITION));
+
+        return builder.build();
+    }
+}
